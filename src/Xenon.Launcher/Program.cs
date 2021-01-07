f1403170e223a1a2fa29cc855b06fa21a53a13e0
ExaInsanity <61018569+ExaInsanity@users.noreply.github.com>
2021-01-07 11:40:00 +0100

    ensure it does not run on macos

--- src/Xenon.Launcher/Program.cs
+++ src/Xenon.Launcher/Program.cs
@@ -11,8 +11,18 @@ namespace Xenon.Launcher
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(String[] args)
+        {
+            if(OperatingSystem.IsMacOS())
+            {
+                Console.WriteLine("Xenon is part of the Helium Toolchain Project which is not supported on MacOS.\n" +
+                    "Press any key to exit the application...");
+                Console.ReadKey();
+                return;
+            }
+
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
