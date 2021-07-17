c23c4ffc0f1612c46bb7354d935a901071246457
ExaInsanity <nicolaiandorfer@outlook.com>
2021-07-17 22:15:31 +0200

    Add installer interface

---
+++ src/Xenon.InstallerLoader/IInstaller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Xenon.InstallerLoader
+{
+    /// <summary>
+    /// Base interface for any installer. Each installer must have one class implementing this interface.
+    /// </summary>
+	public interface IInstaller
+	{
+        /// <summary>
+        /// Called when the installer is loaded.
+        /// </summary>
+        public void Initialize();
+
+        /// <summary>
+        /// Called when the launcher is shut down, in case files need to be closed etc.
+        /// </summary>
+        public void Disable();
+	}
+}
