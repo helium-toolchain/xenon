915f141781783a21999f30339865d2a3e42170dc
ExaInsanity <nicolaiandorfer@outlook.com>
2021-07-17 22:10:23 +0200

    Add semver representation

---
+++ src/Xenon.TypeLib/Version.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Xenon.TypeLib
+{
+#pragma warning disable CS0660
+#pragma warning disable CS0661
+	public struct Version
+#pragma warning restore CS0660
+#pragma warning restore CS0661
+	{
+		public UInt16 Major { get; set; }
+		public UInt16 Minor { get; set; }
+		public UInt16 Hotfix { get; set; }
+
+		public static Boolean operator ==(Version left, Version right)
+		{
+			return left.Major == right.Major
+				&& left.Minor == right.Minor
+				&& left.Hotfix == right.Hotfix;
+		}
+
+		public static Boolean operator !=(Version left, Version right)
+		{
+			return !(left == right);
+		}
+
+		public static Boolean operator >(Version left, Version right)
+		{
+			if(left.Major != right.Major)
+			{
+				return left.Major > right.Major;
+			}
+			if(left.Minor != right.Minor)
+			{
+				return left.Minor > right.Minor;
+			}
+			if(left.Hotfix != right.Hotfix)
+			{
+				return left.Hotfix > right.Hotfix;
+			}
+
+			// they are equal
+			return false;
+		}
+
+		public static Boolean operator <(Version left, Version right)
+		{
+			if(left.Major != right.Major)
+			{
+				return left.Major < right.Major;
+			}
+			if(left.Minor != right.Minor)
+			{
+				return left.Minor < right.Minor;
+			}
+			if(left.Hotfix != right.Hotfix)
+			{
+				return left.Hotfix < right.Hotfix;
+			}
+
+			// they are equal
+			return false;
+		}
+
+		public static Boolean operator >=(Version left, Version right)
+		{
+			if(left.Major != right.Major)
+			{
+				return left.Major > right.Major;
+			}
+			if(left.Minor != right.Minor)
+			{
+				return left.Minor > right.Minor;
+			}
+			if(left.Hotfix != right.Hotfix)
+			{
+				return left.Hotfix > right.Hotfix;
+			}
+
+			// they are equal
+			return true;
+		}
+
+		public static Boolean operator <=(Version left, Version right)
+		{
+			if(left.Major != right.Major)
+			{
+				return left.Major < right.Major;
+			}
+			if(left.Minor != right.Minor)
+			{
+				return left.Minor < right.Minor;
+			}
+			if(left.Hotfix != right.Hotfix)
+			{
+				return left.Hotfix < right.Hotfix;
+			}
+
+			// they are equal
+			return true;
+		}
+	}
+}
