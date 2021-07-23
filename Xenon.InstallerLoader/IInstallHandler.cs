6ed0bdeb9aa830e6abc17480fad4eca3b336c221
ExaInsanity <nicolaiandorfer@outlook.com>
2021-07-23 20:32:34 +0200

    add: specify install directories to the installer

--- Xenon.InstallerLoader/IInstallHandler.cs
+++ Xenon.InstallerLoader/IInstallHandler.cs
@@ -14,11 +14,11 @@ namespace Xenon.InstallerLoader
 		/// <summary>
 		/// Installs the selected version: download files and assets, set up startup command, ...
 		/// </summary>
-        public void Install(String version);
+        public void Install(String version, String installDirectory);
 
 		/// <summary>
 		/// For notchian-derived clients: patch the version.json file to their needs.
 		/// </summary>
-		public void PatchVersionMeta(String version); // only required if this is a client installer - it will not get called for server installers
+		public void PatchVersionMeta(String version, String installDirectory); // only required if this is a client installer - it will not get called for server installers
     }
 }
